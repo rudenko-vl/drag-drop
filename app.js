fb6630d732d089b4b18c495335d288659c3501be
rudenko-vl <vladrudenko7@gmail.com>
2021-11-23 14:27:46 +0200

    change

---
+++ app.js
@@ -0,0 +1,35 @@
+const item = document.querySelector(".item");
+const placeholders = document.querySelectorAll(".placeholder");
+for (const placeholder of placeholders) {
+  placeholder.addEventListener("dragover", dragover);
+  placeholder.addEventListener("dragenter", dragenter);
+  placeholder.addEventListener("dragleave", dragleave);
+  placeholder.addEventListener("drop", dragdrop);
+}
+item.addEventListener("dragstart", dragstart);
+item.addEventListener("dragend", dragend);
+
+function dragstart(event) {
+  event.target.classList.add("hold");
+  setTimeout(() => {
+    event.target.classList.add("hide");
+  }, 0);
+}
+function dragend(event) {
+  event.target.className = "item";
+}
+function dragover(event) {
+  event.preventDefault();
+}
+function dragenter(event) {
+  event.target.classList.add("hovered");
+  console.log("dragenter");
+}
+function dragleave(event) {
+  event.target.classList.remove("hovered");
+  console.log("dragleave");
+}
+function dragdrop(event) {
+  event.target.append(item);
+  event.target.classList.remove("hovered");
+}
